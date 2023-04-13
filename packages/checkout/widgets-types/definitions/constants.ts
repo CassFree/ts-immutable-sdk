d4b86ae0ffad6f6ae553be8b5537d939735c42e9
Chris James <chris@webglowit.net>
2023-04-13 14:18:51 +1000

    wip commit packages

---
+++ packages/checkout/widgets-types/definitions/constants.ts
@@ -0,0 +1,15 @@
+export enum Network {
+  ETHEREUM = 'Ethereum',
+  GOERLI = 'Goerli',
+  POLYGON = 'Polygon',
+}
+
+export enum WidgetTheme {
+  LIGHT = "light",
+  DARK = "dark",
+  CUSTOM = "custom",
+}
+
+export enum ConnectionProviders {
+  METAMASK = "metamask"
+}
